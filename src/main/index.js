7d0111d56592586a20ef9fa92f7ff029ca924768
vaultec81 <47548474+vaultec81@users.noreply.github.com>
2020-06-01 17:47:45 -0700

    Add packager to project

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,8 @@
 import path from 'path';
 import {app, BrowserWindow} from 'electron';
 
-// 加载本地入口页面地址，开发环境下用devServer，所以用的http协议；生产模式下用file协议
 const entryUrl = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:8080/index.html'
+  ? 'http://localhost:6789/index.html'
   : `file://${path.join(__dirname, 'index.html')}`;
 
 let window = null;
@@ -15,7 +14,6 @@ app.on('ready', () => {
 });
 
 app.on('window-all-closed', () => {
-  // mac下比较特殊，所有窗口关闭了也不退出程序
   if(process.platform !== 'darwin') {
     app.quit();
   }
